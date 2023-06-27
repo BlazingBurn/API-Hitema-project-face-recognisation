313f9c161b4a65fb0c352ee7f70cb7ca85095899
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-06-27 21:08:53 +0200

    change openCV to API immaga

---
+++ src/test/java/com/hitema/MaxAirain/APIHitemaprojectfacerecognisation/ApiHitemaProjectFaceRecognisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitema.MaxAirain.APIHitemaprojectfacerecognisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHitemaProjectFaceRecognisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
