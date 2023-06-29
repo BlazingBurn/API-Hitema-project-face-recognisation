16cf6aa6097b039910e4795f24d1732b0351dc24
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-06-29 10:31:20 +0200

    update pom

--- src/main/java/com/hitema/MaxAirain/APIHitemaprojectfacerecognisation/ApiHitemaProjectFaceRecognisationApplication.java
+++ src/main/java/com/hitema/MaxAirain/APIHitemaprojectfacerecognisation/ApiHitemaProjectFaceRecognisationApplication.java
@@ -1,18 +1,9 @@
 package com.hitema.MaxAirain.APIHitemaprojectfacerecognisation;
 
-import com.google.common.io.Resources;
-import com.hitema.MaxAirain.APIHitemaprojectfacerecognisation.Service.FaceRecognitionService;
-import com.hitema.MaxAirain.APIHitemaprojectfacerecognisation.Service.ImageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class ApiHitemaProjectFaceRecognisationApplication {
