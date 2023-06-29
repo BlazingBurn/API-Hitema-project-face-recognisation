ead11babcfe0285b7439bd1f2c6e9b955c461b43
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-06-29 16:23:18 +0200

    Update WebConfig => add allowedMethods

--- src/main/java/com/hitema/MaxAirain/APIHitemaprojectfacerecognisation/Config/WebConfig.java
+++ src/main/java/com/hitema/MaxAirain/APIHitemaprojectfacerecognisation/Config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+                .addMapping("/api/v1/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
